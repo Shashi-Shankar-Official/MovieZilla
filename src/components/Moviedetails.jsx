0882ab23299b75463fdb2d71eb69e511d5248442
SHASHI SHANKAR <sk7698323@gmail.com>
2024-06-07 16:31:02 +0530

    Detail page about to completion

--- src/components/Moviedetails.jsx
+++ src/components/Moviedetails.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { asyncloadmovie, removemovie } from "../store/actions/movieActions";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import { Link, useNavigate } from 'react-router-dom';
 import Loding from "./partials/Loding";
 
 function Moviedetails() {
+    const {pathname} = useLocation();
     const navigate = useNavigate(); 
     const {id} = useParams();
     const {info} = useSelector(state => state.movie);
@@ -34,23 +35,80 @@ function Moviedetails() {
             <a target="_blank" href={info.detail.homepage}><i className="ri-external-link-fill"></i></a>
             <a target="_blank" href={`https://www.wikidata.org/wiki/${info.externalid.wikidata_id}`}><i className="ri-global-line"></i></a>
             <a target="_blank" href={`https://www.imdb.com/title/${info.externalid.imdb_id}`}>imdb</a>
-            </nav>
+            </nav>            
 
             {/* part 2 poster and details */}
             <div className="w-full flex">
-                <div>
-                <img className='shadow-[8px_17px_38px_2px] h-[40vh] object-cover rounded-lg bg-zinc-500' 
+
+                <img className='shadow-[8px_17px_38px_2px] h-[50vh] object-cover rounded-lg bg-zinc-500' 
                 src={`https://image.tmdb.org/t/p/original/${info.detail.poster_path || info.detail.backdrop_path}`} alt="" />
 
-                <div className="mt-5">
+                <div className="content ml-[10%] text-white">
+                    <h1 className="text-5xl font-black  italic">
+                        {info.detail.name || info.detail.title || info.detail.original_name || info.detail.original_title}
+                        <small className="text-2xl font-bold text-zinc-300">({info.detail.release_date.split("-")[0]})</small>
+                    </h1>
+                    
+                    <div className="mt-3 mb-5 flex items-center gap-x-5">
+                    <span className='text-white text-xl font-semibold bg-yellow-500 rounded-full w-[5vh] h-[5vh] flex justify-center items-center'>
+                    {(info.detail.vote_average * 10).toFixed()}<sup>%</sup></span>
+
+                    <h1 className="text-2xl font-semibold w-[60px] leading-6">User Score</h1>
+                    <h1>{info.detail.release_date}</h1>
+                    <h1>{info.detail.genres.map((g) => g.name).join(",")}</h1>
+                    <h1>{info.detail.runtime}min</h1>
+                    </div>
+
+                    <h1 className="text-xl font-semibold italic text-zinc-200">{info.detail.tagline}</h1>
+
+                    <h1 className="text-2xl mb-2 mt-5 ">Overview</h1>
+                    <p className="">{info.detail.overview}</p> 
+
+                    <h1 className="text-2xl mb-2 mt-5 ">Movie Translated</h1>
+                    <p className="mb-10">{info.translations.join(", ")}</p>      
+
+                    <Link className="rounded-lg p-5 bg-[#6556CD] text-xl gap-2 items-center" to={`${pathname}/tailer`} >Play Trailer <i class="ri-play-circle-line"></i></Link>               
+                    
+                     
+                </div>
+            </div>
+
+
+            {/* part 3 Available on Platform */}
+            <div className="w-[80%] gap-y-5  flex flex-col mt-10">
+            
                     {info.watchproviders && 
                         info.watchproviders.flatrate && 
-                        info.watchproviders.flatrate.map(w => <img className="w-[5vh] h-[5vh] object-cover rounded-md"
+                        <div className="flex gap-10 items-center text-zinc-200 ">
+                          <h1 className="text-lg font-semibold">Available on platform</h1>
+                          {info.watchproviders.flatrate.map((w,i) => <img id={i} title={w.provider_name} className="w-[5vh] h-[5vh] object-cover rounded-lg"
                         src={`https://image.tmdb.org/t/p/original/${w.logo_path}`} alt="" />)}
-                </div>
-                </div>
-            
+                        </div>
+                    }
+
+                    {info.watchproviders && 
+                    info.watchproviders.rent && 
+                        <div className="flex gap-x-10 items-center text-zinc-200">
+                          <h1 className="text-lg font-semibold">Available on Rent</h1>
+                          {info.watchproviders.rent.map((w,i) => <img id={i} title={w.provider_name} className="w-[5vh] h-[5vh] object-cover rounded-lg"
+                        src={`https://image.tmdb.org/t/p/original/${w.logo_path}`} alt="" />)}
+                        </div>
+                    }
+
+                    {info.watchproviders && 
+                    info.watchproviders.buy && 
+                        <div className="flex gap-10 items-center text-zinc-200">
+                          <h1 className="text-lg font-semibold">Available to buy</h1>
+                          {info.watchproviders.buy.map((w,i) => <img id={i} title={w.provider_name} className="w-[5vh] h-[5vh] object-cover rounded-lg"
+                        src={`https://image.tmdb.org/t/p/original/${w.logo_path}`} alt="" />)}
+                        </div>
+                    }
+                        
+                
             </div>
+                
+            
+            
 
         </div>
     ) : <Loding />;
--- src/store/actions/movieActions.jsx
+++ src/store/actions/movieActions.jsx
@@ -10,13 +10,15 @@ export const asyncloadmovie = (id) => async (dispatch, getState) => {
          const similar = await axios.get(`/movie/${id}/similar`);
          const videos = await axios.get(`/movie/${id}/videos`);
          const watchproviders = await axios.get(`/movie/${id}/watch/providers`);
+         const translations = await axios.get(`/movie/${id}/translations`);
          let theultimatedetails = {
             detail :detail.data,
             externalid: externalid.data,
             recommendations: recommendations.data.results,
             similar:similar.data.results,
             videos: videos.data.results.find(m => m.type === "Trailer"),
-            watchproviders:watchproviders.data.results.IN
+            watchproviders:watchproviders.data.results.IN,
+            translations: translations.data.translations.map(t => t.english_name),
          };
          dispatch(loadmovie(theultimatedetails));
         //  console.log(theultimatedetails);
