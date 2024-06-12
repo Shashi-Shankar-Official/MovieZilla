0c90514b8e4474b6e2fe8c44d105eb1d0ef8572d
SHASHI SHANKAR <sk7698323@gmail.com>
2024-06-12 16:25:34 +0530

    some changes in ui

--- src/components/partials/Contact.jsx
+++ src/components/partials/Contact.jsx
@@ -11,6 +11,8 @@ function Contact() {
             <h1 className="text-3xl font-semibold text-[#6556CD] ">We are waiting to hear from you </h1>
             <h1 className="text-3xl font-semibold text-zinc-300 ">you can email us at: </h1>
             <h1 className="text-5xl font-semibold text-zinc-300 mt-10"><i className="ri-mail-line"></i>shankarshashi386@gmail.com</h1>
+            <p className="text-3xl font-semibold text-zinc-500 mt-3">or</p>
+            <h1 className="text-5xl font-semibold text-zinc-300 mt-10"><i className="ri-mail-line"></i>ksachin814112@gmail.com</h1>
         </div>
         </>
     )
